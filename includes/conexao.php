ba57c619cf282c608a6ef130c783251c9b6902ab
gigi <giovanah.ps@gmail.com>
2024-07-02 11:38:15 -0300

    cadastrar e mostrar

--- includes/conexao.php
+++ includes/conexao.php
@@ -8,8 +8,8 @@ $port = 3307;
 $con = mysqli_connect($hostname, $username,$password,$database,$port );
 
 if(mysqli_connect_errno()){
-    printf("erro de conexão: %s",
-    mysqli_connect_errno());
+    printf("erro de conexão: %s",mysqli_connect_errno());
+    
     exit;
 }
 
