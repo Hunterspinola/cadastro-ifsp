f4f5ce38786f445a11f879935c7cc1866334eb8b
gi <giovanah.ps@gmail.com>
2024-07-16 12:14:30 -0300

    novas alterações

--- listarCliente.php
+++ listarCliente.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="lista.css">
+   <link rel="stylesheet" href="listarCliente.css">
     <title>Document</title>
 </head>
 <body>
     <?php 
     include('includes/conexao.php');
-    $sql = "SELECT * FROM cidade";
+    $sql = "SELECT * FROM cliente";
     $result = mysqli_query($con,$sql);
     $row = mysqli_fetch_array($result);
     ?>
-    <h1 align="center">Consulte cidade</h1>
+    <h1 align="center">Consulte cliente</h1>
     <table align="center" border="1" width="500">
         <tr>
             <th>Código</th>
             <th>Nome</th>
-            <th>Estado</th>
+            <th>Email</th>
             <th>Alterar</th>
             <th>Deletar</th>
         </tr>
@@ -28,14 +28,14 @@
             echo "<tr>";
             echo "<td>".$row['id']."</td>";
             echo "<td>".$row['nome']."</td>";
-            echo "<td>".$row['estado']."</td>";
-            echo "<td><a href = 'alteraCidade.php?id=".$row['id']."'>Alterar</a></td>";
-            echo "<td><a href = 'deletaCidade.php?id=".$row['id']."'>Deleta</a></td>";
+            echo "<td>".$row['email']."</td>";
+            echo "<td><a href = 'alteraCliente.php?id=".$row['id']."'>Alterar</a></td>";
+            echo "<td><a href = 'deletaCliente.php?id=".$row['id']."'>Deleta</a></td>";
             echo "</tr>";
         }
         ?> 
     </table>
 
-    <a  href="cadastrocidade.html">Cadastrar nova cidade</a>
+    <a  href="cadastrarCliente.html">Cadastrar novo cliente</a>
 </body>
 </html>
